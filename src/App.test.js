d455fb3b9a315e975034485b5136020ccddf2a97
Patricia <patricia.badji89@gmail.com>
2024-12-05 15:49:46 +0100

    starts ex.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,71 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
+import { fireEvent, render, screen } from "@testing-library/react";
+//import App from "./App";
+import Welcome from "./components/Welcome";
+//import AllTheBooks from "./components/AllTheBooks";
+import SingleComment from "./components/SingleComment";
+import BookList from "./components/BookList";
+import SingleBook from "./components/SingleBook";
+import { click } from "@testing-library/user-event/dist/click";
+import { Card } from "react-bootstrap";
+//import { Button } from "react-bootstrap";
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
+//test("renders learn react link", () => {
+//render(<App />);
+//const linkElement = screen.getByText(/learn react/i);
+//expect(linkElement).toBeInTheDocument();
+//});
+
+//verificare che Welcome venga montato - cioè verificare che si veda nella pagina Benvenuti in Epicode
+
+it("shows Welcome alert", () => {
+  render(<Welcome />);
+  const WelcomeMessage = screen.getByText(/benvenuti/i);
+  expect(WelcomeMessage).toBeInTheDocument();
+});
+
+//verificare che il numero delle cards corrisponda al numero dei file nel json. AlltheBooks mappa i libri da fantasy. Che il numero di card sia uguale alla lunghezza del json?
+
+//it("renders same amount of cards as books in json file", () => {
+//render(<AllTheBooks />);
+//const AmountOfCards = screen.queryAllByTestId("test-card");
+
+//expect(AmountOfCards).toHaveLength();
+//});
+
+//verificare che CommentArea venga renderizzato. è async e fa una fetch. Si vede una lista dei commenti. Prendo l'altro componente, che se me lo monta vuol dire che monta comment area!
+
+it("displays CommentArea component correctly", async () => {
+  render(<SingleComment />);
+  const DeleteButton = await screen.findByText(/elimina/i);
+
+  expect(DeleteButton).toBeInTheDocument();
+});
+
+//verifica che la barra di ricerca dei libri funzioni correttamente
+
+it("renders searched book title", async () => {
+  render(<BookList />, <SingleBook />);
+  const input = screen.getAllByPlaceholderText(/cerca un libro/i);
+  fireEvent.change(input, { target: { value: "magician" } });
+  const arrayOfSearchedBooks = await screen.findAllByTestId(
+    "test-searched-book"
+  );
+  expect(arrayOfSearchedBooks).toBeGreaterThan(0);
+});
+
+//verificare che il bordo del libro selezionato cambi colore
+
+it("changes border colour when selected", () => {
+  render(<SingleBook />);
+  const Book = screen.getByTestId("test-searched-book");
+  fireEvent.click(screen.getAllByTestId("test-searched-book"));
+  expect(Book).toHaveStyle("3px solid red");
+});
+
+//7. verificare che all'avvio della pagina non ci sia il componente singlecomment
+
+it("does not display SingleComment component when page loads", () => {
+  render(<BookList />);
+  const SingleComment = screen.queryByText(/elimina/i);
+  expect(SingleComment).toBeNull();
+});
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,22 +1,26 @@
-import { useState } from 'react'
-import SingleBook from './SingleBook'
-import { Col, Form, Row } from 'react-bootstrap'
-import CommentArea from './CommentArea'
+import { useState } from "react";
+import SingleBook from "./SingleBook";
+import { Col, Form, Row } from "react-bootstrap";
+import CommentArea from "./CommentArea";
 
 const BookList = ({ books }) => {
-  const [searchQuery, setSearchQuery] = useState('')
-  const [selectedBook, setSelectedBook] = useState(null)
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedBook, setSelectedBook] = useState(null);
 
   const changeSelectedBook = (asin) => {
-    setSelectedBook(asin)
-  }
+    setSelectedBook(asin);
+  };
 
   return (
     <>
       <Row>
         <Col md={8}>
           <Row className="justify-content-center mt-5">
-            <Col xs={12} md={4} className="text-center">
+            <Col
+              xs={12}
+              md={4}
+              className="text-center"
+            >
               <Form.Group>
                 <Form.Control
                   type="search"
@@ -31,7 +35,11 @@ const BookList = ({ books }) => {
             {books
               .filter((b) => b.title.toLowerCase().includes(searchQuery))
               .map((b) => (
-                <Col xs={12} md={4} key={b.asin}>
+                <Col
+                  xs={12}
+                  md={4}
+                  key={b.asin}
+                >
                   <SingleBook
                     book={b}
                     selectedBook={selectedBook}
@@ -46,7 +54,7 @@ const BookList = ({ books }) => {
         </Col>
       </Row>
     </>
-  )
-}
+  );
+};
 
-export default BookList
+export default BookList;
